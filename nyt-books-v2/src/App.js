a3fd1cfbe7d68812820c70738fbd2c852a7eb723
S Vega P <seba.vegap@gmail.com>
2024-06-08 18:56:22 -0400

    Header funcionando

--- nyt-books-v2/src/App.js
+++ nyt-books-v2/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+//import de react-bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
+//import de componentes
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* aqui iran los componentes, rodeados de browserrouter y navigation, routes y path
+       */}
+       <Home></Home>
     </div>
   );
 }
---
+++ nyt-books-v2/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Navbar bg="dark" variant="dark" sticky="top" expand="lg" className="mb-5 pb-2">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              src="assets/imgs/Logo-Libros1.png"
+              alt=""
+              width="30"
+              height="24"
+              className="d-inline-block align-text-top"
+            />
+            NYT Best Sellers
+          </Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <NavDropdown title="Categories" id="navbarDropdown">
+              <NavDropdown.Item href="#">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#">Something else here</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
