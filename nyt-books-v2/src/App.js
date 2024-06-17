c5720b2051836a8d9f9a1165e59eaaac69b06852
S Vega P <seba.vegap@gmail.com>
2024-06-16 21:14:51 -0400

    footer fix para que sea sticky

--- nyt-books-v2/src/App.js
+++ nyt-books-v2/src/App.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -12,12 +11,14 @@ function App() {
   return (
     <BooksProvider>
       <BrowserRouter>
-        <div className="App">
+        <div className="App d-flex flex-column min-vh-100">
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/book/:bookId" element={<Books />} />
-          </Routes>
+          <main className="flex-grow-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/book/:bookId" element={<Books />} />
+            </Routes>
+          </main>
           <Footer />
         </div>
       </BrowserRouter>
--- nyt-books-v2/src/components/Footer.jsx
+++ nyt-books-v2/src/components/Footer.jsx
@@ -3,43 +3,43 @@ import { Navbar, Container, Row, Col } from 'react-bootstrap';
 import logo from '../assets/imgs/Logo-Libros1.png';
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <Navbar bg="dark" variant="dark" className="pt-3">
-                <Container fluid className="justify-content-around">
-                    <Row>
-                        <Col>
-                            <Navbar.Brand href="#">
-                                <img
-                                    src={logo}
-                                    alt=""
-                                    width="30"
-                                    height="24"
-                                    className="d-inline-block align-text-top"
-                                />
-                                NYT Best Sellers
-                            </Navbar.Brand>
-                        </Col>
-                        <Col>
-                            <div className="navbar-nav text-white">
-                                <p>
-                                    All info provided by{' '}
-                                    <a href="https://developer.nytimes.com/">The New York Times Books API</a>
-                                </p>
-                            </div>
-                        </Col>
-                        <Col>
-                            {/* Optional search input section */}
-                            {/* <form className="d-flex">
+  return (
+    <footer className="footer mt-auto">
+      <Navbar bg="dark" variant="dark" className="pt-3">
+        <Container fluid className="justify-content-around">
+          <Row>
+            <Col>
+              <Navbar.Brand href="#">
+                <img
+                  src={logo}
+                  alt=""
+                  width="30"
+                  height="24"
+                  className="d-inline-block align-text-top"
+                />
+                NYT Best Sellers
+              </Navbar.Brand>
+            </Col>
+            <Col>
+              <div className="navbar-nav text-white">
+                <p>
+                  All info provided by{' '}
+                  <a href="https://developer.nytimes.com/">The New York Times Books API</a>
+                </p>
+              </div>
+            </Col>
+            <Col>
+              {/* Optional search input section */}
+              {/* <form className="d-flex">
                 <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
                 <button className="btn btn-outline-success" type="submit">Buscar</button>
               </form> */}
-                        </Col>
-                    </Row>
-                </Container>
-            </Navbar>
-        </footer>
-    );
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
+    </footer>
+  );
 };
 
 export default Footer;
