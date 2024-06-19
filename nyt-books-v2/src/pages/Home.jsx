3189bf98d09fc7154f74ec587f78578e335ee24a
S Vega P <71044707+sebavegap@users.noreply.github.com>
2024-06-19 16:53:22 -0400

    comentarios home

--- nyt-books-v2/src/pages/Home.jsx
+++ nyt-books-v2/src/pages/Home.jsx
@@ -9,6 +9,8 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { useBooks } from '../context/BooksContext';
 
+//Se establece un máximo de caracteres en el string para la descripción de los libros.
+//Si la descripción supera el máximo, se aplica un slice al string y se añade "..." al final.
 const truncateString = (str, num) => {
   if (str.length <= num) {
     return str;
@@ -16,13 +18,18 @@ const truncateString = (str, num) => {
   return str.slice(0, num) + "...";
 };
 
+//Se crea el componente Home, el cual se encarga de mostrar la página principal de la aplicación.
 const Home = () => {
+  //Se obtienen las funciones de navegación y la ubicación actual.
   const navigate = useNavigate();
   const location = useLocation();
+  //Se obtienen los datos de los libros.
   const { booksData } = useBooks();
+  //Se crea una referencia para cada categoría de libros.
   const categoryRefs = useRef({});
 
   useEffect(() => {
+    //Si la ubicación actual tiene un estado de scroll, se obtiene la referencia de la categoría y se desplaza hasta ella.
     if (location.state?.scrollTo) {
       const categoryElement = categoryRefs.current[location.state.scrollTo];
       if (categoryElement) {
@@ -32,10 +39,14 @@ const Home = () => {
     }
   }, [location, navigate]);
 
+  //Función que se encarga de manejar el click en una tarjeta de libro.
+  //Se navega a la página del libro seleccionado.
+  //Se establece el URL de la página del libro con su codigo isbn y la categoría a la que pertenece.
   const handleCardClick = (book, category) => {
     navigate(`/book/${book.primary_isbn13}`, { state: { book, category } });
   };
 
+  //Se establcen las propiedades de responsive para el carrusel de libros.
   const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
@@ -55,7 +66,9 @@ const Home = () => {
   };
 
   return (
+    //Se establece el contenedor principal de la página.
     <Container fluid style={{ height: 'auto', margin: 0, width: 'auto', padding: 0 }}>
+      {/* Se establece el contenedor del banner principal de la página. */}
       <Container>
         <Row>
           <Col>
@@ -71,8 +84,13 @@ const Home = () => {
       </Container>
 
       <Container className="pt-5 pb-5">
+        {/* Se mapea por cada resultado de la lista de libros, se crea una fila con el nombre de la categoría y se muestra un carrusel con los libros de la categoría. */}
         {booksData && booksData.results.lists.map((category, index) => (
           <Row
+          /* Se establece la clave de la fila con el índice de la categoría. */
+          /* Se establece la clase de la fila con un margen inferior de 5. */
+          /* Se establece la referencia de la fila con el nombre de la categoría. */
+          /* Se establece el título de la categoría. */
             key={index}
             className="mb-5"
             ref={(el) => { categoryRefs.current[category.list_name] = el; }}
